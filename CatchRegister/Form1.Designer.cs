d7be8da3eb8a27dfe2f71c69d889fa5022e3ae9c
Andrew Barber <andrbarb535@ed.amdsb.ca>
2018-10-14 19:26:22 -0400

    Add project files.

---
+++ CatchRegister/Form1.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace CatchRegister
+{
+    partial class catchRegister
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(catchRegister));
+            this.backImage = new System.Windows.Forms.Label();
+            this.flowerPot1 = new System.Windows.Forms.Label();
+            this.firstFlowerPot = new System.Windows.Forms.Label();
+            this.secondFlowerPot = new System.Windows.Forms.Label();
+            this.thirdFlowerPot = new System.Windows.Forms.Label();
+            this.wateringCan = new System.Windows.Forms.Label();
+            this.bag = new System.Windows.Forms.Label();
+            this.sprayBottle = new System.Windows.Forms.Label();
+            this.recordPlayer = new System.Windows.Forms.Label();
+            this.glove = new System.Windows.Forms.Label();
+            this.crazyDaveSpeechBubble = new System.Windows.Forms.Label();
+            this.next = new System.Windows.Forms.Button();
+            this.prev = new System.Windows.Forms.Button();
+            this.licencePlate = new System.Windows.Forms.Label();
+            this.wallet = new System.Windows.Forms.Label();
+            this.firstFlowerPotSoldOut = new System.Windows.Forms.Label();
+            this.secondFlowerPotSoldOut = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // backImage
+            // 
+            this.backImage.Image = ((System.Drawing.Image)(resources.GetObject("backImage.Image")));
+            this.backImage.Location = new System.Drawing.Point(-2, -1);
+            this.backImage.Name = "backImage";
+            this.backImage.Size = new System.Drawing.Size(670, 500);
+            this.backImage.TabIndex = 0;
+            // 
+            // flowerPot1
+            // 
+            this.flowerPot1.AutoSize = true;
+            this.flowerPot1.Location = new System.Drawing.Point(320, 301);
+            this.flowerPot1.Name = "flowerPot1";
+            this.flowerPot1.Size = new System.Drawing.Size(0, 13);
+            this.flowerPot1.TabIndex = 1;
+            // 
+            // firstFlowerPot
+            // 
+            this.firstFlowerPot.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.firstFlowerPot.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.firstFlowerPot.Location = new System.Drawing.Point(339, 229);
+            this.firstFlowerPot.Name = "firstFlowerPot";
+            this.firstFlowerPot.Size = new System.Drawing.Size(56, 16);
+            this.firstFlowerPot.TabIndex = 2;
+            this.firstFlowerPot.Text = "$2,500";
+            this.firstFlowerPot.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.firstFlowerPot.Click += new System.EventHandler(this.firstFlowerPot_Click);
+            // 
+            // secondFlowerPot
+            // 
+            this.secondFlowerPot.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.secondFlowerPot.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.secondFlowerPot.Location = new System.Drawing.Point(401, 229);
+            this.secondFlowerPot.Name = "secondFlowerPot";
+            this.secondFlowerPot.Size = new System.Drawing.Size(56, 16);
+            this.secondFlowerPot.TabIndex = 3;
+            this.secondFlowerPot.Text = "$2,500";
+            this.secondFlowerPot.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.secondFlowerPot.Click += new System.EventHandler(this.secondFlowerPot_Click);
+            // 
+            // thirdFlowerPot
+            // 
+            this.thirdFlowerPot.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.thirdFlowerPot.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.thirdFlowerPot.Location = new System.Drawing.Point(463, 229);
+            this.thirdFlowerPot.Name = "thirdFlowerPot";
+            this.thirdFlowerPot.Size = new System.Drawing.Size(56, 16);
+            this.thirdFlowerPot.TabIndex = 4;
+            this.thirdFlowerPot.Text = "$2,500";
+            this.thirdFlowerPot.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.thirdFlowerPot.Click += new System.EventHandler(this.thirdFlowerPot_Click);
+            // 
+            // wateringCan
+            // 
+            this.wateringCan.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.wateringCan.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.wateringCan.Location = new System.Drawing.Point(525, 229);
+            this.wateringCan.Name = "wateringCan";
+            this.wateringCan.Size = new System.Drawing.Size(58, 16);
+            this.wateringCan.TabIndex = 5;
+            this.wateringCan.Text = "$10,000";
+            this.wateringCan.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.wateringCan.Click += new System.EventHandler(this.wateringCan_Click);
+            // 
+            // bag
+            // 
+            this.bag.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.bag.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bag.Location = new System.Drawing.Point(305, 314);
+            this.bag.Name = "bag";
+            this.bag.Size = new System.Drawing.Size(57, 16);
+            this.bag.TabIndex = 6;
+            this.bag.Text = "$750";
+            this.bag.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.bag.Click += new System.EventHandler(this.bag_Click);
+            // 
+            // sprayBottle
+            // 
+            this.sprayBottle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.sprayBottle.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sprayBottle.Location = new System.Drawing.Point(368, 314);
+            this.sprayBottle.Name = "sprayBottle";
+            this.sprayBottle.Size = new System.Drawing.Size(53, 16);
+            this.sprayBottle.TabIndex = 7;
+            this.sprayBottle.Text = "$1,000";
+            this.sprayBottle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.sprayBottle.Click += new System.EventHandler(this.sprayBottle_Click);
+            // 
+            // recordPlayer
+            // 
+            this.recordPlayer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.recordPlayer.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.recordPlayer.Location = new System.Drawing.Point(427, 314);
+            this.recordPlayer.Name = "recordPlayer";
+            this.recordPlayer.Size = new System.Drawing.Size(59, 16);
+            this.recordPlayer.TabIndex = 8;
+            this.recordPlayer.Text = "$15,000";
+            this.recordPlayer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.recordPlayer.Click += new System.EventHandler(this.recordPlayer_Click);
+            // 
+            // glove
+            // 
+            this.glove.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.glove.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.glove.Location = new System.Drawing.Point(492, 314);
+            this.glove.Name = "glove";
+            this.glove.Size = new System.Drawing.Size(53, 16);
+            this.glove.TabIndex = 9;
+            this.glove.Text = "$1,000";
+            this.glove.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.glove.Click += new System.EventHandler(this.glove_Click);
+            // 
+            // crazyDaveSpeechBubble
+            // 
+            this.crazyDaveSpeechBubble.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.crazyDaveSpeechBubble.Location = new System.Drawing.Point(65, 23);
+            this.crazyDaveSpeechBubble.Name = "crazyDaveSpeechBubble";
+            this.crazyDaveSpeechBubble.Size = new System.Drawing.Size(205, 105);
+            this.crazyDaveSpeechBubble.TabIndex = 12;
+            this.crazyDaveSpeechBubble.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.crazyDaveSpeechBubble.Click += new System.EventHandler(this.crazyDaveSpeechBubble_Click);
+            // 
+            // next
+            // 
+            this.next.BackColor = System.Drawing.Color.Maroon;
+            this.next.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.next.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.next.ForeColor = System.Drawing.Color.Red;
+            this.next.Location = new System.Drawing.Point(510, 332);
+            this.next.Name = "next";
+            this.next.Size = new System.Drawing.Size(73, 31);
+            this.next.TabIndex = 13;
+            this.next.Text = "NEXT";
+            this.next.UseVisualStyleBackColor = false;
+            this.next.Click += new System.EventHandler(this.next_Click);
+            // 
+            // prev
+            // 
+            this.prev.BackColor = System.Drawing.Color.Maroon;
+            this.prev.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.prev.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.prev.ForeColor = System.Drawing.Color.Red;
+            this.prev.Location = new System.Drawing.Point(215, 332);
+            this.prev.Name = "prev";
+            this.prev.Size = new System.Drawing.Size(73, 31);
+            this.prev.TabIndex = 14;
+            this.prev.Text = "PREV";
+            this.prev.UseVisualStyleBackColor = false;
+            this.prev.Click += new System.EventHandler(this.prev_Click);
+            // 
+            // licencePlate
+            // 
+            this.licencePlate.BackColor = System.Drawing.Color.NavajoWhite;
+            this.licencePlate.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.licencePlate.Location = new System.Drawing.Point(305, 431);
+            this.licencePlate.Name = "licencePlate";
+            this.licencePlate.Size = new System.Drawing.Size(107, 60);
+            this.licencePlate.TabIndex = 15;
+            this.licencePlate.Text = "FJPA - 003";
+            this.licencePlate.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.licencePlate.Click += new System.EventHandler(this.licencePlate_Click);
+            // 
+            // wallet
+            // 
+            this.wallet.BackColor = System.Drawing.Color.Sienna;
+            this.wallet.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.wallet.ForeColor = System.Drawing.Color.Lime;
+            this.wallet.Location = new System.Drawing.Point(574, 472);
+            this.wallet.Name = "wallet";
+            this.wallet.Size = new System.Drawing.Size(70, 16);
+            this.wallet.TabIndex = 16;
+            this.wallet.Text = "$50, 000";
+            // 
+            // firstFlowerPotSoldOut
+            // 
+            this.firstFlowerPotSoldOut.BackColor = System.Drawing.Color.Black;
+            this.firstFlowerPotSoldOut.ForeColor = System.Drawing.Color.Transparent;
+            this.firstFlowerPotSoldOut.Location = new System.Drawing.Point(323, 201);
+            this.firstFlowerPotSoldOut.Name = "firstFlowerPotSoldOut";
+            this.firstFlowerPotSoldOut.Size = new System.Drawing.Size(72, 13);
+            this.firstFlowerPotSoldOut.TabIndex = 17;
+            this.firstFlowerPotSoldOut.Text = "SOLD OUT!";
+            this.firstFlowerPotSoldOut.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // secondFlowerPotSoldOut
+            // 
+            this.secondFlowerPotSoldOut.BackColor = System.Drawing.Color.Black;
+            this.secondFlowerPotSoldOut.ForeColor = System.Drawing.Color.Transparent;
+            this.secondFlowerPotSoldOut.Location = new System.Drawing.Point(401, 201);
+            this.secondFlowerPotSoldOut.Name = "secondFlowerPotSoldOut";
+            this.secondFlowerPotSoldOut.Size = new System.Drawing.Size(72, 13);
+            this.secondFlowerPotSoldOut.TabIndex = 18;
+            this.secondFlowerPotSoldOut.Text = "SOLD OUT!";
+            this.secondFlowerPotSoldOut.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // catchRegister
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(984, 500);
+            this.Controls.Add(this.secondFlowerPotSoldOut);
+            this.Controls.Add(this.firstFlowerPotSoldOut);
+            this.Controls.Add(this.wallet);
+            this.Controls.Add(this.licencePlate);
+            this.Controls.Add(this.prev);
+            this.Controls.Add(this.next);
+            this.Controls.Add(this.crazyDaveSpeechBubble);
+            this.Controls.Add(this.glove);
+            this.Controls.Add(this.recordPlayer);
+            this.Controls.Add(this.sprayBottle);
+            this.Controls.Add(this.bag);
+            this.Controls.Add(this.wateringCan);
+            this.Controls.Add(this.thirdFlowerPot);
+            this.Controls.Add(this.secondFlowerPot);
+            this.Controls.Add(this.firstFlowerPot);
+            this.Controls.Add(this.flowerPot1);
+            this.Controls.Add(this.backImage);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "catchRegister";
+            this.Text = "Catch Register";
+            this.Load += new System.EventHandler(this.catchRegister_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label backImage;
+        private System.Windows.Forms.Label flowerPot1;
+        private System.Windows.Forms.Label firstFlowerPot;
+        private System.Windows.Forms.Label secondFlowerPot;
+        private System.Windows.Forms.Label thirdFlowerPot;
+        private System.Windows.Forms.Label wateringCan;
+        private System.Windows.Forms.Label bag;
+        private System.Windows.Forms.Label sprayBottle;
+        private System.Windows.Forms.Label recordPlayer;
+        private System.Windows.Forms.Label glove;
+        private System.Windows.Forms.Label crazyDaveSpeechBubble;
+        private System.Windows.Forms.Button next;
+        private System.Windows.Forms.Button prev;
+        private System.Windows.Forms.Label licencePlate;
+        private System.Windows.Forms.Label wallet;
+        private System.Windows.Forms.Label firstFlowerPotSoldOut;
+        private System.Windows.Forms.Label secondFlowerPotSoldOut;
+    }
+}
+
---
+++ CatchRegister/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace CatchRegister
+{
+    public partial class catchRegister : Form
+    {
+
+        double sum, difference, product, quotient;
+        string mode = "happy";
+
+        public catchRegister()
+        {
+            InitializeComponent();
+        }
+
+        private void catchRegister_Load(object sender, EventArgs e)
+        {
+            Graphics g = this.CreateGraphics();
+            Pen drawPen = new Pen(Color.Black, 10);
+            SolidBrush drawBrush = new SolidBrush(Color.Blue);
+
+            backImage.Visible = true;
+        }
+
+        private void firstFlowerPot_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void secondFlowerPot_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void wateringCan_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bag_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void crazyDaveSpeechBubble_Click(object sender, EventArgs e)
+        {
+            if (mode == "happy")
+            {
+                mode = "sad";
+                crazyDaveSpeechBubble.Text = "I'm Crazy Dave!";
+
+            }
+            else if (mode == "sad")
+            {
+                mode = "angry";
+                crazyDaveSpeechBubble.Text = "My Prices are Crazy!";
+            }
+            else
+            {
+                mode = "happy";
+                crazyDaveSpeechBubble.Text = "Why? \n Because I'm Crazy!";
+            }
+        }
+
+        private void next_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void prev_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void thirdFlowerPot_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void licencePlate_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sprayBottle_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void recordPlayer_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void glove_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
